da2b63b3b13db6f31295c560d75e06439ad90951
Enrico <rico>
2020-10-05 20:05:16 -0300

    fix: remove a level of nesting in CreateTeam

--- src/pages/CreateTeam/index.tsx
+++ src/pages/CreateTeam/index.tsx
@@ -1,5 +1,10 @@
-import TeamConfig from 'components/TeamConfig'
-import TeamInfo from 'components/TeamInfo'
+import PlayerAvatar from 'components/PlayerAvatar'
+import PlayerCard from 'components/PlayerCard'
+import RadioInput from 'components/RadioInput'
+import TagInput from 'components/TagInput'
+import TextArea from 'components/TextArea'
+import TextInput from 'components/TextInput'
+import { PLAYERS } from 'mocks/players'
 import React from 'react'
 import { Col, Grid, Row } from 'styles/grid'
 import SectionTemplate from 'templates/SectionTemplate'
@@ -10,6 +15,8 @@ import * as S from './styles'
 export default function CreateTeam() {
   const { teamToEdit } = useTypedSelector(['teamToEdit'])
 
+  const { name, players, description, tags, website, type } = teamToEdit
+
   console.log({ teamToEdit })
 
   return (
@@ -18,8 +25,91 @@ export default function CreateTeam() {
         <Col size={1}>
           <SectionTemplate title="Create your team">
             <S.Wrapper>
-              <TeamInfo teamToEdit={teamToEdit} />
-              <TeamConfig />
+              <Row>
+                <S.Headline>
+                  <h3>TEAM INFORMATION</h3>
+                </S.Headline>
+              </Row>
+              <Row>
+                <Col size={1}>
+                  <TextInput
+                    label="Team name"
+                    id="team-name"
+                    placeholder="Insert team name"
+                    value={name}
+                  />
+                  <TextArea
+                    label="Description"
+                    id="team-desc"
+                    placeholder="Insert team description"
+                    value={description}
+                  />
+                </Col>
+                <Col size={1}>
+                  <TextInput
+                    label="Team website"
+                    id="team-website"
+                    placeholder="Insert team website"
+                    value={website}
+                  />
+                  <S.Label>Team type</S.Label>
+                  <Row>
+                    <RadioInput
+                      name="team-type"
+                      label="Real"
+                      id="team-type"
+                      checked={type === 'real'}
+                    />
+                    <RadioInput
+                      name="team-type"
+                      label="Fantasy"
+                      id="team-type"
+                      checked={type === 'fantasy'}
+                    />
+                  </Row>
+                  <TagInput label="Tags" id="team-tags" value={tags} />
+                </Col>
+              </Row>
+              <Row>
+                <S.Headline>
+                  <h3>CONFIGURE SQUAD</h3>
+                </S.Headline>
+              </Row>
+              <Row>
+                <Col size={1}>
+                  <div>
+                    Formation <span>3-4-3</span>
+                  </div>
+                  <S.SoccerField>
+                    <S.PlayerRow>
+                      <PlayerAvatar player={PLAYERS[0]} />
+                      <PlayerAvatar player={PLAYERS[2]} />
+                      <PlayerAvatar player={PLAYERS[2]} />
+                    </S.PlayerRow>
+                    <S.FieldCenter />
+                    <S.LineCenter />
+                    <S.PlayerRow>
+                      <PlayerAvatar player={PLAYERS[0]} />
+                      <PlayerAvatar player={PLAYERS[2]} />
+                      <PlayerAvatar player={PLAYERS[2]} />
+                      <PlayerAvatar player={PLAYERS[2]} />
+                    </S.PlayerRow>
+                    <S.PlayerRow>
+                      <PlayerAvatar player={PLAYERS[0]} />
+                      <PlayerAvatar player={PLAYERS[2]} />
+                      <PlayerAvatar player={PLAYERS[2]} />
+                    </S.PlayerRow>
+                    <S.PlayerRow>
+                      <PlayerAvatar player={PLAYERS[0]} />
+                    </S.PlayerRow>
+                  </S.SoccerField>
+                </Col>
+                <Col size={1}>
+                  {PLAYERS.map((player) => (
+                    <PlayerCard player={player} />
+                  ))}
+                </Col>
+              </Row>
             </S.Wrapper>
           </SectionTemplate>
         </Col>
--- src/pages/CreateTeam/styles.ts
+++ src/pages/CreateTeam/styles.ts
@@ -1,5 +1,6 @@
-import styled from 'styled-components'
-import { mediaQuery } from 'styles/helpers'
+import styled, { css } from 'styled-components'
+import { Row } from 'styles/grid'
+import { fontType, mediaQuery, transition } from 'styles/helpers'
 
 export const Wrapper = styled.div`
   padding: 3rem 10rem;
@@ -10,4 +11,94 @@ export const Wrapper = styled.div`
   ${mediaQuery('sm')(`
     padding: 3rem 1rem;
   `)}
+`
+
+export const Headline = styled.div`
+  ${({ theme }) => css`
+    text-align: center;
+    width: 100%;
+    h3 {
+      color: ${theme.colors.grey};
+      font-size: ${theme.font.sizes.medium};
+      ${fontType('b')};
+    }
+  `}
+`
+
+export const Label = styled.div`
+  ${({ theme }) => css`
+      color: ${theme.colors.darkGrey};
+      cursor: text;
+      font-size: ${theme.font.sizes.medium};
+      margin: 2rem 0 1rem;
+
+      ${fontType('b')};
+      ${transition()}
+      ${mediaQuery('sm')(`
+		font-size: ${theme.font.sizes.small};
+		`)}
+    }
+  `}
+`
+
+// Team Info
+export const TeamTypeRow = styled(Row)``
+
+// Team Configuration
+export const SoccerField = styled.div`
+  ${({ theme }) => css`
+    background: ${theme.colors.formationFieldGradient};
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+    border-radius: 4px;
+    position: relative;
+    color: white;
+
+    h2 {
+      font-size: ${theme.font.sizes.large};
+      ${fontType('b')}
+      ${mediaQuery('xs')(`
+        font-size: ${theme.font.sizes.small}
+      `)};
+    }
+  `}
+`
+
+export const PlayerRow = styled.div`
+  ${({ theme }) => css`
+    align-items: center;
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    justify-content: center;
+    padding: 1.8rem;
+    text-align: center;
+  `}
+`
+
+export const FieldCenter = styled.div`
+  ${({ theme }) => css`
+    border-radius: 50%;
+    border: 2px solid ${theme.colors.lightGrey};
+    height: 5rem;
+    width: 5rem;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  `}
+`
+
+export const LineCenter = styled.div`
+  ${({ theme }) => css`
+    /* border-radius: 50%; */
+    border: 1px solid ${theme.colors.lightGrey};
+    height: 1px;
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  `}
 `
